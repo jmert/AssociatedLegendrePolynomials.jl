1fe48bce092565e1c436d5104754202da8d89d3a
Justin Willmert <justin@jdjlab.com>
2020-04-29 15:46:38 -0500

    Make return inference an implementation test, not interface test

--- test/implementation.jl
+++ test/implementation.jl
@@ -1,4 +1,5 @@
 using Random
+import ..Norms
 
 @testset "Equality of legendre[!]" begin
     LMAX = 10
@@ -40,6 +41,30 @@ end
         x = 2rand() - 1
         @test num_deriv1(l, m, x) ≈ dual_deriv1(l, m, x)
         @test num_deriv1(l, m, x) ≈ dual_deriv1_tab(l, m, x)
+    end
+end
+
+@testset "Return inference ($(typeof(N)) with argument type $T)" for N in Norms, T in NumTypes
+    import ..LMAX
+    Λ₀ = fill(0.0)
+    Λ₁ = zeros(LMAX+1)
+    Λ₂ = zeros(LMAX+1, LMAX+1)
+    # 7 cases in recursion to handle:
+    #   1. Initial condition
+    #   2. Boost along diagonal from (even,even) -> (odd,odd)
+    #      2a. Boost in ℓ from (m,m) -> (m+1, m)
+    #      2b. Boost again in ℓ from (m+1,m) -> (m+2, m)
+    #   3. Repeat (2) but for step along diagonal from (odd,odd) -> (even,even)
+    cases = ((0,0), (1,1), (2,1), (3,1), (2,2), (3,2), (4,2))
+    @testset "(ℓ,m) == ($ℓ, $m)" for (ℓ, m) in cases
+        # Return type matches that of the argument, even though internal calculation
+        # will promote.
+        @test @inferred(legendre(N, ℓ, m, one(T))) isa T
+        # Also must handle single value, all ℓ for fixed m, and all (ℓ,m) up to
+        # (LMAX,LMAX)
+        @test @inferred(legendre!(N, Λ₀, LMAX, LMAX, one(T))) isa typeof(Λ₀)
+        @test @inferred(legendre!(N, Λ₁, LMAX, LMAX, one(T))) isa typeof(Λ₁)
+        @test @inferred(legendre!(N, Λ₂, LMAX, LMAX, one(T))) isa typeof(Λ₂)
     end
 end
 
