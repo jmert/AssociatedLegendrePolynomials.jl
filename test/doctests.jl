c11f6a5ee80dfa9f07ab2ccb0a353d7f789e54d8
Justin Willmert <justin@willmert.me>
2021-05-15 13:13:54 -0500

    Limit doctests to Julia >= v1.6 since outputs are version dependent

--- test/doctests.jl
+++ test/doctests.jl
@@ -1,11 +1,14 @@
 using Documenter, Logging
 
-# Disable Documeter's Info logging
-oldlvl = Logging.min_enabled_level(current_logger())
-disable_logging(Logging.Info)
-try
-    DocMeta.setdocmeta!(Legendre, :DocTestSetup, :(using Legendre); recursive=true)
-    doctest(Legendre, testset="Doc Tests")
-finally
-    disable_logging(oldlvl - 1)
+# Outputs are Julia version specific
+if VERSION >= v"1.6"
+    # Disable Documeter's Info logging
+    oldlvl = Logging.min_enabled_level(current_logger())
+    disable_logging(Logging.Info)
+    try
+        DocMeta.setdocmeta!(Legendre, :DocTestSetup, :(using Legendre); recursive=true)
+        doctest(Legendre, testset="Doc Tests")
+    finally
+        disable_logging(oldlvl - 1)
+    end
 end
